68c10e8c4a910f1492dca8ecefe081779e8d6c53
ElonaHoxhaj96 <elona6915@gmail.com>
2024-09-04 21:34:57 +0200

    printed in page

--- db_products.php
+++ db_products.php
@@ -1,19 +1,21 @@
 <?php
 require_once __DIR__ . '/Dog_product.php';
-require_once __DIR__ . '/Cat_product..php';
+require_once __DIR__ . '/Cat_product.php';
+require_once __DIR__ . '/DISCOUNT/Discount.php';
+
 
 $db_products = [
     //PRODOTTI PER CANI 
-    new Dog_product('Crocchette Per Cani Adulti', 'Alimentazione', '€50,00 (12 kg)'),
+    new Dog_product('Crocchette Per Cani Adulti', 'Alimentazione', '€50,00 (12 kg)', '20%'),
     new Dog_product('Cibo Umido per Cani','Alimentazione', '€1,50 (400 g)'),
     new Dog_product('Guinzaglio in Nylon','Accessori','€15,00'),
-    new Dog_product('Collare Regolabile','Accessori',' €10,00'),
+    new Dog_product('Collare Regolabile','Accessori',' €10,00', '30%'),
     new Dog_product('Pettorina per Cani','Accessori','€25,00'),
-    new Dog_product('Giocattolo in Gomma','Giocattoli',' €8,00'),
+    new Dog_product('Giocattolo in Gomma','Giocattoli',' €8,00', '15%'),
     new Dog_product('Pettine per Cani','Toelettatura','€5,00'),
     new Dog_product('Shampoo per Cani','Toelettatura','€12,00 (250 ml)'),
     new Dog_product('Cuscino per Cani','Letti e Accessori','€35,00'),
-    new Dog_product('Trasportino per Cani','Trasporto',' €70,00'),
+    new Dog_product('Trasportino per Cani','Trasporto',' €70,00', '15%'),
 
     //PRODOTTI PER GATTI 
     new Cat_product('Royal Canin Indoor Adult', 'Cibo secco', '€30,00 (2 kg)'),
@@ -28,5 +30,5 @@ $db_products = [
     new Cat_product('Feliway Classic Diffuser','Benessere e comodità','€30,00'),
   
 ];
-var_dump($db_products);
+
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php 
 require_once __DIR__ . '/Dog_product.php';
-require_once __DIR__ . '/Cat_product..php';
-require_once __DIR__ . '/db_products.php'
+require_once __DIR__ . '/Cat_product.php';
+require_once __DIR__ . '/db_products.php';
+require_once __DIR__ . '/./DISCOUNT/Discount.php'
 
 
 ?>
@@ -15,7 +16,17 @@ require_once __DIR__ . '/db_products.php'
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
-    
+    <?php foreach($db_products as $product): ?>
+        <div class="card d-flex flex-row inline" style="width: 18rem;">
+        <div class="card-body">
+            <p class="card-text">Nome Prodotto: <?php echo $product->name?> </p>
+            <p class="card-text">Categoria: <?php echo $product->category ?> </p>
+            <p class="card-text">Prezzo: <?php echo $product->price ?> </p>
+            <p class="card-text">Sconto: <?php echo $product->discount ?? '-' ?> </p>
+
+        </div>
+        </div>
+    <?php endforeach ;?>
 
 
     <!-- BOOTSTRAP JS -->
